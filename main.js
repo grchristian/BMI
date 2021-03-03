cbd6493ff8d6b7f99a6425cc894600c29b79ab74
Christian González <79123808+grchristian@users.noreply.github.com>
2021-03-02 21:29:17 -0600

    Final update
    se le dió el formato final

--- main.js
+++ main.js
@@ -8,30 +8,28 @@ calcular.onclick = function()
 {
     if(peso.value!="" && altura.value!="") {
         imc_calc = (peso.value/(altura.value* altura.value));
-        imc.innerHTML = imc_calc.toFixed(2);
-        console.log(imc_calc);
 
         if(imc_calc<=18.5) {
-            rango.innerHTML = "BAJO DE PESO";
+            alert("BMI = " + imc_calc.toFixed(2) + "\nUnder weight");
         }
         else if(imc_calc>18.5 && imc_calc<=24.9) {
-            rango.innerHTML = "PESO SALUDABLE";
+            alert("BMI = " + imc_calc.toFixed(2) + "\nHealthy weight");
         }
         else if(imc_calc>=25 && imc_calc<=29.9) {
-            rango.innerHTML = "SOBREPESO";
+            alert("BMI = " + imc_calc.toFixed(2) + "\nOverweight");
         }
         else if(imc_calc>=30 && imc_calc<=34.9) {
-            rango.innerHTML = "OBESIDAD 1º";
+            alert("BMI = " + imc_calc.toFixed(2) + "\nObesity 1º");
         }
         else if(imc_calc>=35 && imc_calc<=39.9) {
-            rango.innerHTML = "OBESIDAD 2º";
+            alert("BMI = " + imc_calc.toFixed(2) + "\nbesity 2º");
         }
         else if(imc_calc=40) {
-            rango.innerHTML = "OBESIDAD MÓRBIDA";
+            alert("BMI = " + imc_calc.toFixed(2) + "\nMorbid obesity");
         }
     }
     else {
-        alert("INGRESA TUS DATOS")
+        alert("Enter your data")
     }
 };
 
